4425e4573d8505c6d95dbb8e37fa409e10d92166
Shivachauhan17 <shivanbd2019@gmail.com>
2024-04-29 23:48:34 +0530

    working on schema query and resolvers

--- server/index.ts
+++ server/index.ts
@@ -6,3 +6,32 @@ import db_connection from './utils/connect_db'
 
 db_connection()
 
+interface ICreateUserArgs{
+    username:string,
+    email:string,
+    password:string,
+    confirmPassword:string
+}
+
+
+const typeDefs=`
+    type User{
+        id:ID!
+        username:String!
+        createdAt:String!
+    }
+
+    type Mutation{
+        createUser(username:String!,email:String!,password:String!,confirmPassword:String):User
+    }
+
+`
+
+const resolvers={
+    Mutation:{
+        createUser:async(root,args:ICreateUserArgs)=>{
+            const {username,email,password,confirmPassword}=args
+
+        }
+    }
+}
